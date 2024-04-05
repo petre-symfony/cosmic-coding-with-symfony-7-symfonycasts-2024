e3221f489a432c35b1f7380dbaeedd38a2b77576
petrero <petrero@gmail.com>
2024-04-05 19:59:08 +0300

    5.5 Rendering an Associative Array  - So we're passing a simple number to Twig and printing it. But Twig can handle whatever complex data you throw at it. For example, in the controller, create a new  variable, set to an associative array. Then pass that into the template as a new variable: myShip.
     In the template, add another div... some text and a table to print the data. In the <td>, we can't just print myShip... because printing an associative array doesn't make sense in PHP... and so it doesn't make sense in Twig. You get the famous error about array to string conversion.
    
     What we want is to print the name key on that array. The way we do that looks exactly like JavaScript: myShip.name.
    
     That's it! And... it works. I'll paste in the rest of our template, which prints the other keys from the array. Looking good.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,8 +13,16 @@ class MainController extends AbstractController
     {
         $starshipCount = 457;
 
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
         return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
         ]);
     }
 }
