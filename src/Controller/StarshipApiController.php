d9e432d787cbaa7623ee35c91108bd582353d537
petrero <petrero@gmail.com>
2024-04-08 10:45:32 +0300

    9.3 Autowiring the Logger Service  - So let's do a challenge: let's log something from our controller. Here's a sneak peek into how I approach this problem in my brain:
      Ok, I need to log something! And... logging is work. And... services do work! Thus, there must be a logger service that I can use! Quod erat demonstrandum!
    
     Forgive me latin nerds. The point is: if we want to log something, we just need to find the service that does that work. Okay! Rerun the command but search for log:
    
      php bin/console debug:autowiring log
     Boom! It found about 10 services, all starting with Psr\Log\LoggerInterface. We're going to talk about what these other services are in the next tutorial. For now, focus on the main one. This tells me is that there is a service in the container for a logger. And to get it, we can autowire it using this interface.
    
     What does that mean? In the controller method where we want the logger, add an argument type-hinted with LoggerInterface - hit tab - then say $logger.
    
     In this case, the name of the argument isn't important: it could be anything. What matters is that the LoggerInterface - that corresponds to this use statement - matches the Psr\Log\LoggerInterface from debug:autowiring.
    
     It's that simple! Symfony will see this type-hint and say:
    
      Oh! Since that type-hint matches the autowiring type for this service, they must want me to pass them that service object.
    
     I don't know why Symfony sounds like a frog in my head. Anyway, let's see if this works. Add dd($logger): dd() stands for "dump and die" and comes from Symfony.
    
     Refresh! Yes! It printed the object beautifully then stopped execution. It's working! Symfony passes us a Monolog\Logger object, which implements that LoggerInterface.
    
     The trick we just did - called autowiring - works in exactly two places: our controller methods and the __construct() method of any service. We'll see that second situation in the next chapter.
    
     Controlling how Services Behave
     - And if you're wondering where this Logger service came from in the first place... we already know the answer! From a bundle. In this case, MonologBundle. And... how could we configure that service... to, I don't know, log to a different file? The answer is: config/packages/monolog.yaml.
    
     This config - path: "%kernel.logs_dir%/%kernel.environment%.log" - configures MonologBundle... which really means that it configures how the services work that MonologBundle give us. We'll learn about this percent syntax in the next tutorial, but this tells the Logger service to log to this dev.log file.

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipApiController extends AbstractController {
 	#[Route('/api/starships')]
-	public function getCollection(): Response {
+	public function getCollection(LoggerInterface $logger): Response {
+		dd($logger);
 		$starships = [
 			new Starship(
 				1,
