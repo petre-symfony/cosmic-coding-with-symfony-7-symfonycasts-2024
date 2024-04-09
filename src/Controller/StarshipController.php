e3fd200dff9c5616a5fcdc3e7c59a435be719c6b
petrero <petrero@gmail.com>
2024-04-09 13:09:32 +0300

    12.4 Linking Between Pages  - Next question: from the homepage, how could we add a link to the new ship show page? The most obvious option is to hardcode the URL, like /starships/ then the id. But there's a better way. Instead, we're going to tell Symfony:
      Hey, I want to generate a URL to this route.
    
     The advantage is that if we decide later to change the URL of this route, every link to this will update automatically.
    
     Let me show you. Find your terminal and run:
    
      php bin/console debug:router
     I haven't mentioned it yet, but every route has an internal name. Right now, they're being auto-generated by Symfony, which is fine. But as soon as you want to generate a URL to a route, we should take control of that name to make sure it never changes.
    
     Find the show page route and add a name key. I'll use app_starship_show.
    
     The name could be anything, but this is the convention I follow: app because it's a route that I'm making in my app, then the controller class name and method name.
    
     Naming a route doesn't change how it works. But it does let us generate a URL to it. Open up templates/main/homepage.html.twig. Down here, turn the ship name into a link. I'll put this onto multiple lines and add an a tag with href="". To generate the URL, say {{ path() }} and pass it the name of the route. I'll put the closing tag on the other side.
    
     If we stop now, this won't quite work. On the homepage:
    
      Some mandatory parameters are missing - id - to generate a URL for route app_starship_show.
    
     That makes sense! We're telling Symfony:
    
      Howdy! I want to generate a URL to this route.
    
     Symfony then responds:
    
      Cool... except that this route has a wildcard in it. So like... what do you want me to put in the URL for the id part?

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController {
-	#[Route('/starships/{id<\d+>}')]
+	#[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
 	public function show(int $id, StarshipRepository $repository): Response {
 		$ship = $repository->find($id);
 
