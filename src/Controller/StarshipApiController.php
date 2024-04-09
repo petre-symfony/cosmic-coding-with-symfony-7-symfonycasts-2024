f3dc22ded1fd3e043347a60ff7d5fa5739ae7426
petrero <petrero@gmail.com>
2024-04-09 12:27:57 +0300

    11.5 Prefixing Every Route URL  - Ok, I have one more routing trick to show you... and this is a fun one! Every route in this controller starts with the same URL: /api/starships. Having the full URL in each route is fine. But if we want, we can automatically prefix each route's URL. Above the class, add a #[Route] attribute with /api/starships.
     Unlike when we put this above a method, this does not create a route. It just says: every route in this class should be prefixed with this URL. So for the first route, remove the path entirely. And for the second, we only need the wildcard part.
    
     Try debug:router again... and watch these URLs:
    
      php bin/console debug:router
     They don't change!

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,21 +2,20 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
 use App\Repository\StarshipRepository;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController {
-	#[Route('/api/starships', methods: ['GET'])]
+	#[Route('', methods: ['GET'])]
 	public function getCollection(StarshipRepository $repository): Response {
 		$starships = $repository->findAll();
 		return $this->json($starships);
 	}
 
-	#[Route('/api/starships/{id<\d+>}', methods: ['GET'])]
+	#[Route('/{id<\d+>}', methods: ['GET'])]
 	public function get($id): Response {
 		dd($id);
 	}
