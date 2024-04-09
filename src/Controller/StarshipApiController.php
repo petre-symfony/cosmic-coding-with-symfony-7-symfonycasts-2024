d316921b99354c642ad2287ccf5ccb35b780997b
petrero <petrero@gmail.com>
2024-04-09 12:21:33 +0300

    11.4 For that reason, it's common in an API to add a methods option set to an array, with GET or POST. I'll do the same thing down here: methods: ['GET'].
     I can't easily test this in a browser, but if we made a POST request to /api/starships/2, it would not match our route.
    
     But we can see the change in our terminal. Run:
    
      php bin/console debug:router
     Perfect! Most routes match any method... but our two API routes only match if a GET request is made to that URL.

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -10,18 +10,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipApiController extends AbstractController {
-	#[Route('/api/starships')]
+	#[Route('/api/starships', methods: ['GET'])]
 	public function getCollection(StarshipRepository $repository): Response {
 		$starships = $repository->findAll();
 		return $this->json($starships);
 	}
 
-	#[Route('/api/starships')]
-	public function create() {
-
-	}
-	
-	#[Route('/api/starships/{id<\d+>}')]
+	#[Route('/api/starships/{id<\d+>}', methods: ['GET'])]
 	public function get($id): Response {
 		dd($id);
 	}
