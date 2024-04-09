b7043a8de105ba810e17a1324073aee2722e99e9
petrero <petrero@gmail.com>
2024-04-09 12:42:19 +0300

    11.7 Triggering a 404 Page  - But try an id that does not exist in our fake database - like /200. The word null is... not what we want. In this situation, we should return a response with a 404 status code.
     To do that, we're going to follow a common pattern: query for an object, then check if it returned anything. If it did not return something, trigger a 404. Do that with throw $this->createNotFoundException(). I'll pass this a message.
    
     Notice the throw keyword: we're throwing a special exception that triggers a 404. That's nice because, as soon as it hits this line, nothing after will be executed.
    
    Try it out! Yes! A 404 response! The message - "Starship not found" - is only shown to developers in dev mode. In production, a totally different page - or JSON - would be returned. You can check the docs for details on production error pages.
    
     Next: let's build the HTML version of this page, a page that shows details about a single starship. Then we'll learn how to link between pages using the route name.

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -19,6 +19,10 @@ class StarshipApiController extends AbstractController {
 	public function get($id, StarshipRepository $repository): Response {
 		$starship = $repository->find($id);
 
+		if (!$starship) {
+			throw $this->createNotFoundException('Starship not found');
+		}
+
 		return $this->json($starship);
 	}
 }
